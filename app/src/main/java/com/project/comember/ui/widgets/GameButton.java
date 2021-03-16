ac903aca9bcf9378c55c754b26297422d17c75b4
Max Seeliger <maximilian.seeliger@gmail.com>
2021-03-16 19:07:12 +0100

    Changed Button Design in order to better highlight the Color
    Signed-off-by: Max Seeliger <maximilian.seeliger@gmail.com>

--- app/src/main/java/com/project/comember/ui/widgets/GameButton.java
+++ app/src/main/java/com/project/comember/ui/widgets/GameButton.java
@@ -1,9 +1,12 @@
 package com.project.comember.ui.widgets;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -16,9 +19,13 @@ import com.project.comember.game.GameColor;
 
 public class GameButton extends View {
 
+    private static final int STANDARD_STROKE_SIZE = 10;
+    private static final int HIGHLIGHT_STROKE_SIZE = 20;
+
     private GameColor mGameColor;
 
     private Paint mCircleColor;
+    private int mStrokeSize = STANDARD_STROKE_SIZE;
 
     private final Paint mMainColor;
     private final Paint mHighlightColor;
@@ -44,15 +51,15 @@ public class GameButton extends View {
         mHighlightColor = new Paint(Paint.ANTI_ALIAS_FLAG);
         mBorderColor = new Paint(Paint.ANTI_ALIAS_FLAG);
 
+        mBorderColor.setColor(Color.WHITE);
+
         mCircleColor = mMainColor;
 
         TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.GameButton, defStyleAttr, defStyleRes);
 
         try {
-
             mMainColor.setColor(attributes.getColor(R.styleable.GameButton_mainColor, 0));
             mHighlightColor.setColor(attributes.getColor(R.styleable.GameButton_highlightColor, 0));
-            mBorderColor.setColor(attributes.getColor(R.styleable.GameButton_borderColor, 0));
         } finally {
             attributes.recycle();
         }
@@ -73,21 +80,52 @@ public class GameButton extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(getWidth(), getHeight(), getWidth(), mCircleColor);
+        int maxX = getWidth();
+        int maxY = getHeight();
+        float rectWidth = mStrokeSize - STANDARD_STROKE_SIZE / 2f;
+        canvas.drawCircle(maxX, maxY, maxX, mBorderColor);
+        canvas.drawCircle(maxX, maxY, maxX - mStrokeSize, mCircleColor);
+        canvas.drawRect(maxX - rectWidth, 0, maxX, maxY, mBorderColor);
+        canvas.drawRect(0, maxY - rectWidth, maxX, maxY, mBorderColor);
     }
 
 
     public void highlight() {
-        changeActiveColor(mHighlightColor);
+        changeActiveColor(mHighlightColor, false);
+        changeStrokeSize(HIGHLIGHT_STROKE_SIZE);
     }
 
     public void unhighlight() {
-        changeActiveColor(mMainColor);
+        changeActiveColor(mMainColor, false);
+        changeStrokeSize(STANDARD_STROKE_SIZE);
     }
 
     private void changeActiveColor(Paint color) {
+        changeActiveColor(color, true);
+    }
+
+    private void changeActiveColor(Paint color, boolean inval) {
         mCircleColor = color;
-        invalidate();
+        if (inval)
+            invalidate();
+    }
+
+    private void changeStrokeSize(int strokeSize) {
+        ((Activity) getContext()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ValueAnimator strokeAnimator = ValueAnimator.ofInt(mStrokeSize, strokeSize);
+                strokeAnimator.setDuration(100);
+                strokeAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        mStrokeSize = (int) animation.getAnimatedValue();
+                        postInvalidate();
+                    }
+                });
+                strokeAnimator.start();
+            }
+        });
     }
 
     public void setGameColor(GameColor gameColor) {
